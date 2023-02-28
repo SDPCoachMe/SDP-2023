ca7f316ee12258d099435b89619d657aafd30500
Thymian18 <damian.kopp01@gmail.com>
2023-02-28 11:07:20 +0100

    Add maps activity where the map is launched at EPFL location

--- app/src/main/java/com/github/sdpcoachme/GreetingActivity.java
+++ app/src/main/java/com/github/sdpcoachme/GreetingActivity.java
@@ -3,10 +3,15 @@ package com.github.sdpcoachme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class GreetingActivity extends AppCompatActivity {
+public class GreetingActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mapButton;
+    Intent mapIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,22 @@ public class GreetingActivity extends AppCompatActivity {
         String str = intent.getStringExtra("name");
 
         receiverMessage.setText(str);
+        mapButton = findViewById(R.id.open_maps_btn);
+        mapButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        /*if (v.getId() == R.id.open_maps_btn) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.open_maps_btn, new MapsFragment()).commit();
+        }*/
+        Uri gmIntentUri = Uri.parse("geo:46.520536,6.568318");
+        //mapIntent = new Intent(GreetingActivity.this, MapsActivity.class);
+        mapIntent = new Intent(Intent.ACTION_VIEW, gmIntentUri);
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+
     }
 }
