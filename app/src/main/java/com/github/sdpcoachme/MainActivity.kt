2467431154df48136448f25e72891ed91c0a303b
lindsaybordier <lindsay.bordier@epfl.ch>
2023-03-01 22:09:52 +0100

    Steps 1 and 2 done. Get and Set Buttons working properly.

--- app/src/main/java/com/github/sdpcoachme/MainActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -14,17 +15,92 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import com.github.sdpcoachme.ui.theme.CoachMeTheme
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.CompletableFuture
+
 
 class MainActivity : ComponentActivity() {
+
+    private val db: DatabaseReference = Firebase.database.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CoachMeTheme {
-                GreetingForm()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    FirebaseForm()
+                    GreetingForm()
+                }
             }
         }
     }
+
+
+    @Composable
+    fun FirebaseForm() {
+        var phoneText by remember { mutableStateOf("") }
+        var emailText by remember { mutableStateOf("") }
+        Column(//modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(//modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(modifier = Modifier.testTag("getButton"),
+                    onClick = {
+                        val future = CompletableFuture<String>()
+
+                        db.child(phoneText).get().addOnSuccessListener {
+                            if (it.value == null) future.completeExceptionally(NoSuchFieldException())
+                            else future.complete(it.value as String)
+                        }.addOnFailureListener {
+                            future.completeExceptionally(it)
+                        }
+
+                        future.thenAccept {
+                            emailText = it
+                        }
+                    },)
+                {
+                    Text("get")
+                }
+                Button(modifier = Modifier.testTag("setButton"),
+                    onClick = {
+                        db.child(phoneText).setValue(emailText)
+                    },)
+                {
+                    Text("set")
+                }
+
+            }
+            TextField(
+                modifier = Modifier.testTag("textfield"),
+                value = phoneText,
+                onValueChange = { phoneText = it },
+                label = { Text("Phone") }
+            )
+            TextField(
+                modifier = Modifier.testTag("textfield"),
+                value = emailText,
+                onValueChange = { emailText = it },
+                label = { Text("Email") }
+            )
+
+        }
+
+    }
 }
+
+
 
 @Composable
 fun GreetingForm() {
@@ -32,7 +108,7 @@ fun GreetingForm() {
     val context = LocalContext.current
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        //modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
