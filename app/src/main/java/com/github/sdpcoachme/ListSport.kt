7f1a1e4ab732fa599c76feb3834e8f3e1a44405b
lindsaybordier <lindsay.bordier@epfl.ch>
2023-03-07 14:44:53 +0100

    Code for multiselection of sport done.
    This can be useful for many other things. Maybe for the location if we want to restrain the location to a few towns at first.

---
+++ app/src/main/java/com/github/sdpcoachme/ListSport.kt
@@ -0,0 +1,6 @@
+package com.github.sdpcoachme
+
+data class ListSport(
+    val title: String,
+    val selected: Boolean
+)
--- app/src/main/java/com/github/sdpcoachme/MainActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoachMeTheme {
                 AccountForm()
+                //MultiSelectList()
             }
         }
     }
@@ -85,6 +86,52 @@ fun AccountForm() {
                 //todo
             })
         { Text("REGISTER") }
+    }
+}
+
+@Composable
+fun MultiSelectList() {
+    var sportItems by remember {
+        mutableStateOf(
+            listOf("Ski", "Tennis", "Padel").map {
+                ListSport(
+                    title = it,
+                    selected = false
+                )
+            }
+        )
+    }
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(sportItems.size) { i ->
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        sportItems = sportItems.mapIndexed { j, item ->
+                            if (i == j) {
+                                item.copy(selected = !item.selected)
+                            } else {
+                                item
+                            }
+                        }
+                    }
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = sportItems[i].title)
+                if (sportItems[i].selected) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Selected",
+                        tint = Color.Green,
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
