c2e8d4ff8674dc918bc67c71bc305902ee04f040
Thymian18 <damian.kopp01@gmail.com>
2023-03-08 17:44:25 +0100

    Add title and modifiable textfields

--- app/src/main/java/com/github/sdpcoachme/EditProfilActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/EditProfilActivity.kt
@@ -8,10 +8,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,63 +42,100 @@ fun Profile() {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ) {
+        TitleRow()
+        EmailRow()
+        FirstNameRow()
+        LastNameRow()
+
         Row (
-            modifier = Modifier.absolutePadding(20.dp, 20.dp, 0.dp, 10.dp),
+            modifier = Modifier.absolutePadding(20.dp, 10.dp, 0.dp, 10.dp),
             verticalAlignment = Alignment.Top,
             horizontalArrangement = Arrangement.Start
         ) {
-            Text(
-                text = "My Profile",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-        Row (
-            modifier = Modifier.absolutePadding(20.dp, 80.dp, 0.dp, 10.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.Start
-        ){
-            Text(text = "Email: ")
-            // replace this Text with read-only TextField?
-            Text(
-                text = "damian.kopp@epfl.ch",
-                modifier = Modifier.absolutePadding(80.dp, 0.dp, 0.dp, 0.dp),
-            )
-        }
-        Row (
-            modifier = Modifier.absolutePadding(20.dp, 10.dp, 0.dp, 10.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.Start
-        ){
-            Text(text = "First name: ")
-            TextField(
-                modifier = Modifier.absolutePadding(40.dp, 0.dp, 0.dp, 0.dp)
-                    .height(40.dp).width(150.dp),
-                value = "Damian",
-                onValueChange = {
-
-                },
-                singleLine = true,
-                maxLines = 1
-            )
-        }
-        Row (
-            modifier = Modifier.absolutePadding(20.dp, 10.dp, 0.dp, 10.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.Start
-        ){
-            Text(text = "Last name: ")
-        }
-        Row (
-            modifier = Modifier.absolutePadding(20.dp, 10.dp, 0.dp, 10.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.Start
-        ){
             Text(text = "Favorite sport: ")
         }
-
     }
+}
 
+@Composable
+fun TitleRow() {
+    Row (
+        modifier = Modifier.absolutePadding(20.dp, 20.dp, 0.dp, 10.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Text(
+            text = "My Profile",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun EmailRow() {
+    Row (
+        modifier = Modifier.absolutePadding(20.dp, 80.dp, 0.dp, 10.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ){
+        Text(text = "Email: ")
+        // replace this Text with read-only TextField?
+        Text(
+            text = "damian.kopp@epfl.ch",
+            modifier = Modifier.absolutePadding(80.dp, 0.dp, 0.dp, 0.dp),
+        )
+    }
+}
+
+@Composable
+fun FirstNameRow() {
+    // bind this to database
+    var fname by remember { mutableStateOf(TextFieldValue("Damian")) }
+
+    Row (
+        modifier = Modifier.absolutePadding(20.dp, 10.dp, 20.dp, 10.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ){
+        Text(text = "First name: ")
+        TextField(
+            modifier = Modifier
+                .absolutePadding(40.dp, 0.dp, 0.dp, 0.dp)
+                .defaultMinSize(150.dp, 40.dp),
+            value = fname,
+            onValueChange = {
+                fname = it
+            },
+            singleLine = true,
+            maxLines = 1
+        )
+    }
+}
+
+@Composable
+fun LastNameRow() {
+    // bind this to database
+    var lname by remember { mutableStateOf(TextFieldValue("Kopp")) }
+
+    Row (
+        modifier = Modifier.absolutePadding(20.dp, 10.dp, 0.dp, 10.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ){
+        Text(text = "Last name: ")
+        TextField(
+            modifier = Modifier
+                .absolutePadding(40.dp, 0.dp, 0.dp, 0.dp)
+                .defaultMinSize(150.dp, 40.dp),
+            value = lname,
+            onValueChange = {
+                lname = it
+            },
+            singleLine = true,
+            maxLines = 1
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/github/sdpcoachme/GreetingActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/GreetingActivity.kt
@@ -1,15 +1,18 @@
 package com.github.sdpcoachme
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import com.github.sdpcoachme.ui.theme.CoachMeTheme
 
@@ -26,11 +29,18 @@ class GreetingActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    val context = LocalContext.current
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(modifier = Modifier.testTag("text"), text = "Hello $name!")
+        Button(onClick = {
+            val intent = Intent(context, EditProfilActivity::class.java)
+            context.startActivity(intent)
+        }) {
+            Text(text = "Edit profile")
+        }
     }
 }
