bcde803186dde9501036cceac1e79da68fc8f030
Thymian18 <damian.kopp01@gmail.com>
2023-03-10 10:38:15 +0100

    Revert commit to correct functionality of bootcamp

--- app/src/main/java/com/github/sdpcoachme/MapsActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/MapsActivity.kt
@@ -8,13 +8,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.sdpcoachme.ui.theme.CoachMeTheme
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
