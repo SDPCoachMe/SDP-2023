0afb4ac3cf106bd7694643f21965b55e9dad3cb2
lindsaybordier <lindsay.bordier@epfl.ch>
2023-03-10 13:21:44 +0100

    Fix dependency to use MockDatabase in testing.

--- app/src/androidTest/java/com/github/sdpcoachme/CoachMeTestApplication.kt
+++ app/src/androidTest/java/com/github/sdpcoachme/CoachMeTestApplication.kt
@@ -3,4 +3,8 @@ package com.github.sdpcoachme
 class CoachMeTestApplication : CoachMeApplication() {
     // For DI in testing, add reference to mocks here
     override var database: Database = MockDatabase()
+    override fun onCreate() {
+        super.onCreate()
+        database = MockDatabase()
+    }
 }
