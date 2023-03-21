d535c095086e39fece4ca016cceead7964011d71
LucaS <luca.engel@epfl.ch>
2023-03-21 13:52:00 +0100

    adapt dashboard activity to use error handling
    redirect to error handling page when no email passed in intent

--- app/src/androidTest/java/com/github/sdpcoachme/DashboardActivityTest.kt
+++ app/src/androidTest/java/com/github/sdpcoachme/DashboardActivityTest.kt
@@ -3,7 +3,7 @@ package com.github.sdpcoachme
 import android.content.Intent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.unit.height
 import androidx.compose.ui.unit.width
 import androidx.test.core.app.ActivityScenario
@@ -27,61 +27,102 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class DashboardActivityTest {
+    private val EXISTING_EMAIL = "example@email.com"
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<DashboardActivity>()
+    val composeTestRule = createEmptyComposeRule()
 
     @Test
     fun drawerOpensOnMenuClick() {
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
-        composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+        val email = EXISTING_EMAIL
+        val launchDashboard = Intent(
+            ApplicationProvider.getApplicationContext(),
+            DashboardActivity::class.java
+        )
+        launchDashboard.putExtra("email", email)
+        ActivityScenario.launch<DashboardActivity>(launchDashboard).use {
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+            composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+        }
     }
 
     @Test
     fun drawerReactsOnCorrectSwipe() {
-        // opens on right swipe
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
-        composeTestRule.onRoot().performTouchInput { swipeRight() }
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
-        // closes on left swipe
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
-        composeTestRule.onRoot().performTouchInput { swipeLeft() }
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+        val email = EXISTING_EMAIL
+        val launchDashboard = Intent(
+            ApplicationProvider.getApplicationContext(),
+            DashboardActivity::class.java
+        )
+        launchDashboard.putExtra("email", email)
+        ActivityScenario.launch<DashboardActivity>(launchDashboard).use {
+            // opens on right swipe
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+            composeTestRule.onRoot().performTouchInput { swipeRight() }
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+            // closes on left swipe
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+            composeTestRule.onRoot().performTouchInput { swipeLeft() }
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+        }
     }
 
     @Test
     fun drawerIgnoresInvalidSwipe() {
-        // ignores left swipe if closed
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
-        composeTestRule.onRoot().performTouchInput { swipeLeft() }
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
-        // ignores right swipe if opened
-        composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
-        composeTestRule.onRoot().performTouchInput { swipeRight() }
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+        val email = EXISTING_EMAIL
+        val launchDashboard = Intent(
+            ApplicationProvider.getApplicationContext(),
+            DashboardActivity::class.java
+        )
+        launchDashboard.putExtra("email", email)
+        ActivityScenario.launch<DashboardActivity>(launchDashboard).use {
+            // ignores left swipe if closed
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+            composeTestRule.onRoot().performTouchInput { swipeLeft() }
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
+            // ignores right swipe if opened
+            composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
+            composeTestRule.onRoot().performTouchInput { swipeRight() }
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+        }
     }
 
     @Test
     fun drawerClosesOnOutsideTouch() {
-        val width = composeTestRule.onRoot().getBoundsInRoot().width
-        val height = composeTestRule.onRoot().getBoundsInRoot().height
-        composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
-        composeTestRule.onRoot().performTouchInput {
-            click(position = Offset(width.toPx() - 10, height.toPx() / 2))
+        val email = EXISTING_EMAIL
+        val launchDashboard = Intent(
+            ApplicationProvider.getApplicationContext(),
+            DashboardActivity::class.java
+        )
+        launchDashboard.putExtra("email", email)
+        ActivityScenario.launch<DashboardActivity>(launchDashboard).use {
+            val width = composeTestRule.onRoot().getBoundsInRoot().width
+            val height = composeTestRule.onRoot().getBoundsInRoot().height
+            composeTestRule.onNodeWithTag(HAMBURGER_MENU).performClick()
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsDisplayed()
+            composeTestRule.onRoot().performTouchInput {
+                click(position = Offset(width.toPx() - 10, height.toPx() / 2))
+            }
+            composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
         }
-        composeTestRule.onNodeWithTag(DRAWER_HEADER).assertIsNotDisplayed()
     }
 
     @Test
     fun drawerBodyContainsClickableMenu() {
-        composeTestRule.onNodeWithTag(MENU_LIST).onChildren().assertAll(hasClickAction())
+        val email = EXISTING_EMAIL
+        val launchDashboard = Intent(
+            ApplicationProvider.getApplicationContext(),
+            DashboardActivity::class.java
+        )
+        launchDashboard.putExtra("email", email)
+        ActivityScenario.launch<DashboardActivity>(launchDashboard).use {
+            composeTestRule.onNodeWithTag(MENU_LIST).onChildren().assertAll(hasClickAction())
+        }
     }
 
     @Test
     fun dashboardDisplaysCorrectEmailFromReceivedIntent() {
-        val email = "john.lennon@gmail.com"
+        val email = EXISTING_EMAIL
         val launchDashboard = Intent(
             ApplicationProvider.getApplicationContext(),
             DashboardActivity::class.java
@@ -91,7 +132,11 @@ class DashboardActivityTest {
             composeTestRule.onNodeWithTag(DASHBOARD_EMAIL).assert(hasText(text = email))
         }
     }
-    private fun dashboardCorrectlyRedirectsOnMenuItemClick(userEmail: String, tag: String, intentMatcher: Matcher<Intent>) {
+    private fun dashboardCorrectlyRedirectsOnMenuItemClick(
+        userEmail: String,
+        tag: String,
+        intentMatcher: Matcher<Intent>
+    ) {
         val launchDashboard = Intent(
             ApplicationProvider.getApplicationContext(),
             DashboardActivity::class.java
@@ -106,7 +151,7 @@ class DashboardActivityTest {
     }
     @Test
     fun dashboardCorrectlyRedirectsOnProfileClick() {
-        val email = "john.lennon@gmail.com"
+        val email = EXISTING_EMAIL
         dashboardCorrectlyRedirectsOnMenuItemClick(
             email,
             PROFILE,
@@ -119,7 +164,7 @@ class DashboardActivityTest {
     @Test
     fun dashboardCorrectlyRedirectsOnLogOutClick() {
         dashboardCorrectlyRedirectsOnMenuItemClick(
-            "jack.daniels@outlook.com",
+            EXISTING_EMAIL,
             LOGOUT,
             hasComponent(LoginActivity::class.java.name)
         )
