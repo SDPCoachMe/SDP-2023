a1cd53f6bce198d63d81d9a6da15f1232577cdc2
LucaS <luca.engel@epfl.ch>
2023-03-21 13:48:59 +0100

    add error handling activity

---
+++ app/src/androidTest/java/com/github/sdpcoachme/errorhandling/IntentExtrasErrorActivityTest.kt
@@ -0,0 +1,88 @@
+package com.github.sdpcoachme.errorhandling
+
+import android.content.Intent
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import com.github.sdpcoachme.EditProfileActivity
+import com.github.sdpcoachme.LoginActivity
+import com.github.sdpcoachme.errorhandling.IntentExtrasErrorActivity.TestTags.Buttons.Companion.GO_TO_LOGIN_BUTTON
+import com.github.sdpcoachme.errorhandling.IntentExtrasErrorActivity.TestTags.TextFields.Companion.ERROR_MESSAGE_FIELD
+import org.junit.Rule
+import org.junit.Test
+
+class IntentExtrasErrorActivityTest {
+
+    @get:Rule
+    val composeTestRule = createEmptyComposeRule()
+
+    @Test
+    fun whenLaunchedWithoutExtraItDisplaysGenericErrorMessage() {
+        val genericErrorMsg = "An error occurred. Please return to the login page and retry."
+
+        val emptyErrorIntent = Intent(ApplicationProvider.getApplicationContext(), IntentExtrasErrorActivity::class.java)
+
+        ActivityScenario.launch<EditProfileActivity>(emptyErrorIntent).use {
+
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertExists()
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertTextEquals(genericErrorMsg)
+
+            composeTestRule.onNodeWithTag(GO_TO_LOGIN_BUTTON).assertExists()
+        }
+    }
+
+    @Test
+    fun whenLaunchedWithExtraItDisplaysThePassedErrorMessage() {
+        val errorMsg = "This is a test error message."
+        val emptyErrorIntent = Intent(ApplicationProvider.getApplicationContext(), IntentExtrasErrorActivity::class.java)
+        emptyErrorIntent.putExtra("errorMsg", errorMsg)
+
+        ActivityScenario.launch<EditProfileActivity>(emptyErrorIntent).use {
+
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertExists()
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertTextEquals(errorMsg)
+
+            composeTestRule.onNodeWithTag(GO_TO_LOGIN_BUTTON).assertExists()
+        }
+    }
+
+    @Test
+    fun goToLoginButtonClickAfterEmptyIntentRedirectsToLoginPage() {
+        val emptyErrorIntent = Intent(ApplicationProvider.getApplicationContext(), IntentExtrasErrorActivity::class.java)
+
+        ActivityScenario.launch<EditProfileActivity>(emptyErrorIntent).use {
+            Intents.init()
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertExists()
+
+            composeTestRule.onNodeWithTag(GO_TO_LOGIN_BUTTON).assertExists()
+                .performClick()
+
+            Intents.intended(hasComponent(LoginActivity::class.java.name))
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun goToLoginButtonClickAfterIntentWithExtraRedirectsToLoginPage() {
+        val emptyErrorIntent = Intent(ApplicationProvider.getApplicationContext(), IntentExtrasErrorActivity::class.java)
+        val errorMsg = "This is a test error message."
+        emptyErrorIntent.putExtra("errorMsg", errorMsg)
+
+        ActivityScenario.launch<EditProfileActivity>(emptyErrorIntent).use {
+            Intents.init()
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertExists()
+            composeTestRule.onNodeWithTag(ERROR_MESSAGE_FIELD).assertTextEquals(errorMsg)
+
+            composeTestRule.onNodeWithTag(GO_TO_LOGIN_BUTTON).assertExists()
+                .performClick()
+
+            Intents.intended(hasComponent(LoginActivity::class.java.name))
+            Intents.release()
+        }
+    }
+}
---
+++ app/src/main/java/com/github/sdpcoachme/errorhandling/IntentExtrasErrorActivity.kt
@@ -0,0 +1,78 @@
+package com.github.sdpcoachme.errorhandling
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import com.github.sdpcoachme.LoginActivity
+import com.github.sdpcoachme.ui.theme.CoachMeTheme
+
+/**
+ * Activity that handles errors related to intent extras.
+ *
+ * It displays the error message received or a generic
+ * error message if none was received. Then the user can
+ * go back to the login activity.
+ */
+class IntentExtrasErrorActivity : ComponentActivity() {
+    class TestTags {
+        class TextFields {
+            companion object {
+                const val ERROR_MESSAGE_FIELD = "errorMessage"
+            }
+        }
+        class Buttons {
+            companion object {
+                const val GO_TO_LOGIN_BUTTON = "goToLoginButton"
+            }
+        }
+    }
+
+    private val GENERIC_ERROR_MESSAGE = "An error occurred. Please return to the login page and retry."
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO handle the null better here
+        val errorMsg = intent.getStringExtra("errorMsg") ?: GENERIC_ERROR_MESSAGE
+
+        setContent {
+            CoachMeTheme {
+                IntentExtrasErrorScreen(errorMsg)
+            }
+        }
+    }
+
+    @Composable
+    fun IntentExtrasErrorScreen(errorMsg: String) {
+        val context = LocalContext.current
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = errorMsg,
+                Modifier.testTag(TestTags.TextFields.ERROR_MESSAGE_FIELD))
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(modifier = Modifier.testTag(TestTags.Buttons.GO_TO_LOGIN_BUTTON),
+                onClick = {
+                    val intent = Intent(context, LoginActivity::class.java)
+                    startActivity(intent)
+                }) {
+                    Text(text = "Return to login page")
+                }
+        }
+    }
+}
