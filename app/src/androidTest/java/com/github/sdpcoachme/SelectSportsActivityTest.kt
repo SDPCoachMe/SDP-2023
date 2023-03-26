4a90b7fc0cbc00130efc032f7776f9c436070ec1
lindsaybordier <lindsayski@gmail.com>
2023-03-26 14:10:43 +0200

    Add some new tests and Make all tests pass.

--- app/src/androidTest/java/com/github/sdpcoachme/SelectSportsActivityTest.kt
+++ app/src/androidTest/java/com/github/sdpcoachme/SelectSportsActivityTest.kt
@@ -5,12 +5,14 @@ import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.*
 import com.github.sdpcoachme.data.Sports
 import com.github.sdpcoachme.data.UserInfo
 import junit.framework.TestCase
+import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,14 +22,25 @@ import java.util.concurrent.TimeUnit
 open class SelectSportsActivityTest {
 
     val email: String = "test@email.com"
+    val userInfo = UserInfo(
+        firstName = "John",
+        lastName = "Doe",
+        email = email,
+        location = "Paris",
+        phone = "0123456789",
+        sports = listOf()
+    )
+    val database = (InstrumentationRegistry.getInstrumentation()
+        .targetContext.applicationContext as CoachMeApplication).database
+    val launchSignup = Intent(ApplicationProvider.getApplicationContext(),
+        SelectSportsActivity::class.java)
+        .putExtra("email", email)
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<SelectSportsActivity>()
 
     @Test
-    fun iconsIniallyNotDisplayed() {
-        val launchSignup = Intent(ApplicationProvider.getApplicationContext(), SelectSportsActivity::class.java)
-        launchSignup.putExtra("email", email)
+    fun tickIconsInitiallyNotDisplayed() {
         ActivityScenario.launch<SignupActivity>(launchSignup).use {
             SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
                 composeTestRule.onNodeWithTag(it.ICON).assertDoesNotExist()
@@ -35,24 +48,35 @@ open class SelectSportsActivityTest {
         }
     }
 
-    // todo comprendre pourquoi le test ne passe pas
     @Test
-    fun iconsDisplayedAfterClick() {
-        val launchSignup = Intent(ApplicationProvider.getApplicationContext(), SelectSportsActivity::class.java)
-        launchSignup.putExtra("email", email)
+    fun tickIconsDisplayedAfterClick() {
         ActivityScenario.launch<SignupActivity>(launchSignup).use {
             SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
                 composeTestRule.onNodeWithTag(it.ROW).performClick()
-                composeTestRule.onNodeWithTag(it.ICON).assertExists()
+            }
+            SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
+                composeTestRule.onNodeWithTag(it.ICON, useUnmergedTree = true).assertIsDisplayed()
+            }
+        }
+    }
+
+    @Test
+    fun tickIconsDisappearAfterSecondClick() {
+        ActivityScenario.launch<SignupActivity>(launchSignup).use {
+            SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
+                composeTestRule.onNodeWithTag(it.ROW).performClick()
+            }
+            SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
+                composeTestRule.onNodeWithTag(it.ROW).performClick()
+            }
+            SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
+                composeTestRule.onNodeWithTag(it.ICON, useUnmergedTree = true).assertDoesNotExist()
             }
         }
     }
 
     @Test
     fun correctInitialScreenContent() {
-        val launchSignup = Intent(ApplicationProvider.getApplicationContext(),
-            SelectSportsActivity::class.java)
-        launchSignup.putExtra("email", email)
         ActivityScenario.launch<SignupActivity>(launchSignup).use {
             SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST.forEach {
                 composeTestRule.onNodeWithTag(it.ROW).assertIsDisplayed()
@@ -65,18 +89,37 @@ open class SelectSportsActivityTest {
     }
 
     @Test
-    fun userInfoUpdatedWithSelectedSports() {
-        val userInfo = UserInfo(
-            firstName = "John",
-            lastName = "Doe",
-            email = email,
-            location = "Paris",
-            phone = "0123456789",
-            sports = listOf()
-        )
-        val database = (InstrumentationRegistry.getInstrumentation()
-            .targetContext.applicationContext as CoachMeApplication).database
+    fun userInfoSelectedSportCorrectlyReplaced() {
+        // Note works only if there are at least 2 sports
+        val userInfo = userInfo.copy(sports = listOf(Sports.values()[1]))
+        val updatedUser =
+            database.addUser(userInfo)
+                .thenApply {
+                    val launchSignup = Intent(ApplicationProvider.getApplicationContext(),
+                        SelectSportsActivity::class.java)
+                    launchSignup.putExtra("email", email)
+                    ActivityScenario.launch<SignupActivity>(launchSignup).use {
+                        composeTestRule.onNodeWithTag(SelectSportsActivity.TestTags.MultiSelectListTag.ROW_TEXT_LIST[0].ROW)
+                            .performClick()
+                        composeTestRule.onNodeWithTag(SelectSportsActivity.TestTags.Buttons.REGISTER)
+                            .performClick()
+                    }
+                }.thenCompose {
+                    database.getUser(email)
+                }.get(10, TimeUnit.SECONDS)
 
+        // Check that the user has the first sport
+        assertThat(updatedUser.sports, hasItem(Sports.values()[0]))
+        // Check that the user does not have the other sports
+        Sports.values().toList().subList(1, Sports.values().size).forEach {
+            assertThat(updatedUser.sports, not(hasItem(it)))
+        }
+
+
+    }
+
+    @Test
+    fun userInfoUpdatedWithAllSelectedSports() {
         val updatedUser =
             database.addUser(userInfo)
                 .thenApply {
