1389b5ed7adc056e73fda70df5f3743019a9c9b8
Ayskin <yann.ennassih@epfl.ch>
2023-04-17 11:29:14 +0200

    removed MapViewModel and refactor MapActivity accordingly
    MapActivity knows holds the whole location retrieval pipeline.

--- app/src/main/java/com/github/sdpcoachme/map/MapActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/map/MapActivity.kt
@@ -1,21 +1,19 @@
 package com.github.sdpcoachme.map
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.core.content.ContextCompat
 import com.github.sdpcoachme.CoachMeApplication
 import com.github.sdpcoachme.Dashboard
-import com.github.sdpcoachme.data.MapState
 import com.github.sdpcoachme.errorhandling.ErrorHandlerLauncher
 import com.github.sdpcoachme.firebase.database.Database
 import com.github.sdpcoachme.map.MapActivity.TestTags.Companion.MAP
@@ -27,6 +25,10 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 
+/**
+ * Main map activity, launched after login. This activity contains the map view and holds
+ * the current last known user location.
+ */
 class MapActivity : ComponentActivity() {
 
     class TestTags {
@@ -38,35 +40,8 @@ class MapActivity : ComponentActivity() {
     private lateinit var database: Database
     private lateinit var email: String
 
-    /**
-     * Create an activity for result : display window to request asked permission.
-     * If granted, launches the callback (here getDeviceLocation(...) which retrieves the user's
-     * location). The contract is a predefined "function" which takes a permission as input and
-     * outputs if the user has granted it or not.
-     */
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                isGranted: Boolean -> if (isGranted) {
-            mapViewModel.getDeviceLocation(fusedLocationProviderClient)
-        } else {
-            // TODO Permission denied or only COARSE given
-        }
-        }
-
-    /**
-     * This function updates the location state of the MapViewModel if the permission is granted.
-     * If the permission is denied, it requests it.
-     */
-    private fun getLocation() =
-        when (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            PackageManager.PERMISSION_GRANTED -> {
-                mapViewModel.getDeviceLocation(fusedLocationProviderClient)
-            }
-            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private val mapViewModel: MapViewModel by viewModels()
+    private var lastUserLocation: MutableState<LatLng?> = mutableStateOf(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,7 +51,6 @@ class MapActivity : ComponentActivity() {
 
         // gets user location at map creation
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        getLocation()
 
         if (email.isEmpty()) {
             val errorMsg = "The map did not receive an email address.\nPlease return to the login page and try again."
@@ -85,11 +59,61 @@ class MapActivity : ComponentActivity() {
             setContent {
                 CoachMeTheme {
                     val dashboardContent: @Composable (Modifier) -> Unit = { modifier ->
-                        Map(modifier = modifier, mapState = mapViewModel.mapState.value)
+                        Map(modifier = modifier, lastUserLocation = lastUserLocation)
                     }
                     Dashboard(dashboardContent, email)
                 }
             }
+        }
+        getLocation()
+    }
+
+    /**
+     * Create an activity for result : display window to request asked permission.
+     * If granted, launches the callback (here getDeviceLocation(...) which retrieves the user's
+     * location). The contract is a predefined "function" which takes a permission as input and
+     * outputs if the user has granted it or not.
+     */
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                getDeviceLocation(fusedLocationProviderClient)
+            } else {
+            // TODO Permission denied or only COARSE given
+            }
+        }
+
+    /**
+     * This function updates the state of lastUserLocation if the permission is granted.
+     * If the permission is denied, it requests it.
+     */
+    private fun getLocation() =
+        when (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_GRANTED -> {
+                getDeviceLocation(fusedLocationProviderClient)
+            }
+            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+
+    @SuppressLint("MissingPermission") //permission is checked before the call
+    private fun getDeviceLocation(fusedLocationProviderClient: FusedLocationProviderClient) {
+        try {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    if (task.result != null) {
+                        println("Last location has been successfully retrieved")
+                        lastUserLocation.value = LatLng(
+                            task.result.latitude,
+                            task.result.longitude
+                        )
+                    } else {
+                        println("Location is disabled on the device")
+                        // TODO handle this case
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            error("getDeviceLocation was called without correct permissions : ${e.message}")
         }
     }
 }
@@ -102,7 +126,7 @@ class MapActivity : ComponentActivity() {
  * location data. The MapState is updated by the MapViewModel in the DashboardActivity.
  */
 @Composable
-fun Map(modifier: Modifier, mapState: MapState) {
+fun Map(modifier: Modifier, lastUserLocation: MutableState<LatLng?>) {
 
     val campus = LatLng(46.520536,6.568318)
     val cameraPositionState = rememberCameraPositionState {
@@ -115,21 +139,21 @@ fun Map(modifier: Modifier, mapState: MapState) {
             .testTag(MAP),
         cameraPositionState = cameraPositionState,
         properties = MapProperties(
-            isMyLocationEnabled = mapState.lastKnownLocation != null,
+            isMyLocationEnabled = lastUserLocation.value != null,
             mapType = MapType.NORMAL
         )
     ) {
         // moves camera to last known location
-        if (mapState.lastKnownLocation != null) {
+        if (lastUserLocation.value  != null) {
             // relaunches the effect when the key changes. So here only once
             LaunchedEffect(true) {
                 cameraPositionState.animate(
-                    CameraUpdateFactory.newLatLngZoom(mapState.lastKnownLocation, 17f)
+                    CameraUpdateFactory.newLatLngZoom(lastUserLocation.value!!, 17f)
                 )
             }
         }
 
         // places a marker at the last known position, if location is null, places at campus
-        Marker(state = MarkerState(mapState.lastKnownLocation?:campus))
+        Marker(state = MarkerState(lastUserLocation.value ?:campus))
     }
 }
