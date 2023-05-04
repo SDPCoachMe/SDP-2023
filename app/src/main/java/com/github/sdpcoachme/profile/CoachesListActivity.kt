08234383f16dc25043ff92e1b96ee86506cb9c7f
Ayskin <yann.ennassih@epfl.ch>
2023-05-04 19:38:24 +0200

    Add filtering sport in nearby coaches

--- app/src/main/java/com/github/sdpcoachme/profile/CoachesListActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/profile/CoachesListActivity.kt
@@ -11,36 +11,52 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Tune
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.github.sdpcoachme.CoachMeApplication
 import com.github.sdpcoachme.R
+import com.github.sdpcoachme.data.Sports
 import com.github.sdpcoachme.data.UserInfo
 import com.github.sdpcoachme.errorhandling.ErrorHandlerLauncher
 import com.github.sdpcoachme.location.provider.FusedLocationProvider.Companion.CAMPUS
 import com.github.sdpcoachme.messaging.ChatActivity
+import com.github.sdpcoachme.profile.CoachesListActivity.TestTags.Buttons.Companion.FILTER
 import com.github.sdpcoachme.ui.Dashboard
 import com.github.sdpcoachme.ui.theme.CoachMeTheme
+import com.github.sdpcoachme.ui.theme.Purple200
 import kotlinx.coroutines.future.await
+import java.util.*
 import java.util.concurrent.CompletableFuture
 
 class CoachesListActivity : ComponentActivity() {
+
+    class TestTags {
+        class Buttons {
+            companion object {
+                const val FILTER = "filterSearch"
+            }
+        }
+    }
+
     // Allows to notice testing framework that the activity is ready
     var stateLoading = CompletableFuture<Void>()
+
+    private lateinit var sportsFilter: MutableState<List<Sports>>
+    private lateinit var selectSportsHandler: (Intent) -> CompletableFuture<List<Sports>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -49,6 +65,10 @@ class CoachesListActivity : ComponentActivity() {
         val lastLocation = (application as CoachMeApplication).locationProvider.getLastLocation()
         val userLatLng = lastLocation.value?: CAMPUS
         val email = database.getCurrentEmail()
+
+        // initially all sports are selected
+        sportsFilter = mutableStateOf(Sports.values().toList())
+        selectSportsHandler = SelectSportsActivity.getHandler(this)
 
         if (email.isEmpty()) {
             val errorMsg = "The coach list did not receive an email address.\nPlease return to the login page and try again."
@@ -70,6 +90,7 @@ class CoachesListActivity : ComponentActivity() {
             setContent {
                 var listOfCoaches by remember { mutableStateOf(listOf<UserInfo>()) }
 
+
                 // Proper way to handle result of a future in a Composable.
                 // This makes sure the listOfCoaches state is updated only ONCE, when the future is complete
                 // This is because the code in LaunchedEffect(true) will only be executed once, when the
@@ -88,95 +109,131 @@ class CoachesListActivity : ComponentActivity() {
 
                 CoachMeTheme {
                     Dashboard(title) {
-                        Column(modifier = it.fillMaxSize()) {
-                            LazyColumn {
-                                items(listOfCoaches) {user ->
-                                    UserInfoListItem(user, isViewingContacts)
-                                }
-                            }
-                        }
+                        CoachesList(it, listOfCoaches, isViewingContacts, sportsFilter)
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun UserInfoListItem(user: UserInfo, isViewingContacts: Boolean) {
-    val context = LocalContext.current
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                if (isViewingContacts) {
-                    val displayChatIntent = Intent(context, ChatActivity::class.java)
-                    displayChatIntent.putExtra("toUserEmail", user.email)
-                    context.startActivity(displayChatIntent)
-                } else {
-                    val displayCoachIntent = Intent(context, ProfileActivity::class.java)
-                    displayCoachIntent.putExtra("email", user.email)
-                    displayCoachIntent.putExtra("isViewingCoach", true)
-                    context.startActivity(displayCoachIntent)
+    @Composable()
+    fun CoachesList(
+        modifier: Modifier,
+        listOfCoaches: List<UserInfo>,
+        isViewingContacts: Boolean,
+        sportsFilter: MutableState<List<Sports>>
+    ) {
+        val context = LocalContext.current
+
+        Box(modifier = modifier.fillMaxSize()) {
+            LazyColumn {
+                items(listOfCoaches) {user ->
+                    if (!Collections.disjoint(user.sports, sportsFilter.value)) {
+                        UserInfoListItem(user, isViewingContacts)
+                    }
                 }
             }
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .height(100.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        // TODO: might be a good idea to merge the profile picture code used here and the one used in EditProfileActivity
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "${user.firstName} ${user.lastName}'s profile picture",
-            modifier = Modifier
-                .size(80.dp)
-                .clip(CircleShape)
-                .border(2.dp, Color.Gray, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text(
-                text = "${user.firstName} ${user.lastName}",
-                style = MaterialTheme.typography.h6,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            FloatingActionButton(
+                onClick = {
+//                    this@CoachesListActivity.sportsFilter.value =
+                    selectSportsHandler(
+                            SelectSportsActivity.getIntent(
+                                context = context,
+                                title = "Filter coaches by sport",
+                                initialValue = sportsFilter.value
+                            )).thenApply {sportsFilter.value = it }
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+                    .testTag(FILTER),
+                backgroundColor = Purple200
             ) {
                 Icon(
-                    imageVector = Icons.Default.Place,
-                    tint = Color.Gray,
-                    contentDescription = "${user.firstName} ${user.lastName}'s location",
-                    modifier = Modifier.size(20.dp)
+                    imageVector = Default.Tune,
+                    contentDescription = "Filter nearby coaches by sport"
                 )
-                Spacer(modifier = Modifier.width(4.dp))
-                // Temporary, until we implement proper location handling
-                Text(
-                    text = user.address.name,
-                    color = Color.Gray,
-                    style = MaterialTheme.typography.body2,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                user.sports.map {
-                    Icon(
-                        imageVector = it.sportIcon,
-                        tint = Color.Gray,
-                        contentDescription = it.sportName,
-                        modifier = Modifier.size(20.dp)
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                }
             }
         }
     }
-    Divider()
+
+    @Composable
+    fun UserInfoListItem(user: UserInfo, isViewingContacts: Boolean) {
+        val context = LocalContext.current
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    if (isViewingContacts) {
+                        val displayChatIntent = Intent(context, ChatActivity::class.java)
+                        displayChatIntent.putExtra("toUserEmail", user.email)
+                        context.startActivity(displayChatIntent)
+                    } else {
+                        val displayCoachIntent = Intent(context, ProfileActivity::class.java)
+                        displayCoachIntent.putExtra("email", user.email)
+                        displayCoachIntent.putExtra("isViewingCoach", true)
+                        context.startActivity(displayCoachIntent)
+                    }
+                }
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .height(100.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // TODO: might be a good idea to merge the profile picture code used here and the one used in EditProfileActivity
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "${user.firstName} ${user.lastName}'s profile picture",
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = "${user.firstName} ${user.lastName}",
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Default.Place,
+                        tint = Color.Gray,
+                        contentDescription = "${user.firstName} ${user.lastName}'s location",
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    // Temporary, until we implement proper location handling
+                    Text(
+                        text = user.address.name,
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.body2,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    user.sports.map {
+                        Icon(
+                            imageVector = it.sportIcon,
+                            tint = Color.Gray,
+                            contentDescription = it.sportName,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                    }
+                }
+            }
+        }
+        Divider()
+    }
 }
+
