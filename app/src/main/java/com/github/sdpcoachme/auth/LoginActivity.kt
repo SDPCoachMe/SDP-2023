4793d24df1e588aae39322a52684f9ede5c09911
LucaS <luca.engel@epfl.ch>
2023-05-24 15:44:01 +0200

    update login page to include logo, welcome text, and little fact

--- app/src/main/java/com/github/sdpcoachme/auth/LoginActivity.kt
+++ app/src/main/java/com/github/sdpcoachme/auth/LoginActivity.kt
@@ -10,17 +10,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.github.sdpcoachme.CoachMeApplication
@@ -152,13 +167,46 @@ class LoginActivity : ComponentActivity() {
         }
 
         Column(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(top = 35.dp) // Adjust the top padding as needed
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            // Your content here
             if (showUI) {
+                Text(
+                    text = "Welcome to\nCoach Me!",
+                    style = MaterialTheme.typography.h1,
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Medium,
+                    textAlign = TextAlign.Center
+                )
 
+                Spacer(modifier = Modifier.height(80.dp))
 
+                Image(
+                    painter = painterResource(id = android.R.drawable.ic_menu_myplaces),
+                    contentDescription = "Coach Me Logo",
+                    modifier = Modifier
+                        .padding(bottom = 32.dp)
+                        .size(200.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .border(1.dp, Color.Black, CircleShape)
+                )
+
+                Spacer(modifier = Modifier.height(80.dp))
+
+                Text(text = "Did you know?\nCoach Me is the best app to track your progress!",
+                    style = MaterialTheme.typography.h2,
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    textAlign = TextAlign.Center
+                )
             } else {
                 // While we are waiting for the next activity to launch, we display a loading
                 CircularProgressIndicator()
